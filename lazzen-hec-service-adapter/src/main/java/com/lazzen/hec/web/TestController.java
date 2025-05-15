f262d1230b5a3d48c9d4eb2a83b1b5e0c10a8750
caszhou <zhouxiajie@qq.com>
2025-05-15 08:47:01 +0800

    feat: better project.

--- lazzen-hec-service-adapter/src/main/java/com/lazzen/hec/web/TestController.java
+++ lazzen-hec-service-adapter/src/main/java/com/lazzen/hec/web/TestController.java
@@ -1,10 +1,20 @@
 package com.lazzen.hec.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sipa.boot.java8.common.dtos.ResponseWrapper;
+
 /**
  * @author caszhou
  * @date 2025/5/15
  */
-@
+@RestController
+@RequestMapping("/test")
 public class TestController {
-    //
+    @PostMapping("/1")
+    public ResponseWrapper<?> test1() {
+        return ResponseWrapper.success();
+    }
 }
