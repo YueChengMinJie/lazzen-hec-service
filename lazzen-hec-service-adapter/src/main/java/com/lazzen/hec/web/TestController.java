3961f8390089ef39913ba4a8c86c07be7504d7ff
caszhou <zhouxiajie@qq.com>
2025-05-15 09:04:19 +0800

    feat: better project.

--- lazzen-hec-service-adapter/src/main/java/com/lazzen/hec/web/TestController.java
+++ lazzen-hec-service-adapter/src/main/java/com/lazzen/hec/web/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sipa.boot.java8.common.dtos.ResponseWrapper;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 /**
  * @author caszhou
  * @date 2025/5/15
@@ -14,6 +16,7 @@ import com.sipa.boot.java8.common.dtos.ResponseWrapper;
 @RequestMapping("/test")
 public class TestController {
     @PostMapping("/1")
+    @Operation(summary = "测试1", description = "测试1")
     public ResponseWrapper<?> test1() {
         return ResponseWrapper.success();
     }
